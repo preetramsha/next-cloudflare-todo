e1c65656b1cc5ea867a26096a392a751ba774ea5
Preet Ramsha <preetramsha123@gmail.com>
2024-08-06 22:10:44 +0530

    minor testing

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,8 +1,12 @@
 'use client';
+
 import Link from 'next/link';
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
+import Cookies from 'universal-cookie';
 export default function test() {
-  
+  const cookie = new Cookies();
+  const [token, setToken] = useState('')
+  const [resp, setResp] = useState('')
      const validateToken = async () => {
         let resp = await fetch(`http://localhost:8787/api/validatetoken`,{
           cache:'default',
@@ -16,16 +20,20 @@ export default function test() {
         return await resp.json();
       }
       useEffect(() => {
-        validateToken().then(r => console.log(r))
+        validateToken().then(r => setResp(r.data.username))
+        setToken(cookie.get('token',{doNotParse:true}));
       }, [])
       
     return (
         <div>
-            <div>{}</div>
+          <div>Here is fetched token</div>
+
+            <p>{resp}</p>
             <br />
             rich nigga shit I do a lotta
+            <br />
             <Link href={'/test2'}>Go to test 2</Link>
-            
+            <br />
             <Link href={'/'}>Home</Link>
         </div>
     );
